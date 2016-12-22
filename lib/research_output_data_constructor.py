bca8a3d984d8071ad46c0731c52ae4fbfe217ebb
vinsburg <vinsburg@gmail.com>
2016-12-22 18:01:22 +0200

    Updated jdistance headers and pep8 compliance

--- lib/research_output_data_constructor.py
+++ lib/research_output_data_constructor.py
@@ -20,13 +20,13 @@ def students_per_category_line_list_constructor(students_per_category_matrix):
     # constructing the row headers and inserting the values in students_per_category_matrix
     categories = len(students_per_category_matrix)
     for category_index in range(categories):
-        return_list += ['category_' + str(category_index + 1)] #indexed
+        return_list += ['category_' + str(category_index + 1)]  # indexed
         return_list += [str(students_per_category_matrix[0][category_index])]
         return_list += [str(students_per_category_matrix[1][category_index])]
         return_list += [str(students_per_category_matrix[2][category_index])]
-        return_list +=  newrow_cell
-
+        return_list += newrow_cell
     return return_list
+
 
 def students_line_list_constructor(worksheet):
     """
@@ -45,21 +45,22 @@ def students_line_list_constructor(worksheet):
     header_row1 = [''] * 7 + ["round_1"] + [''] * 43 + ["round_2"] + [''] * 43 + ["round_3"] + [''] * 43
     roundstring1 = ['google_result_rating'] + [''] * 19 + ['selections_per_category'] + [''] * 9 + [''] + [
         'average_google_category_rank'] + [''] * 12
-    header_row2 = [''] * 4 + ["jdistances"] + [''] * 2 + roundstring1 * 3
-    roundstring2 = [str(a_cell) for a_cell in google_categories] + [str(i + 1) for i in range(10)] + ['jdistance'] + [str(i + 1) for i in range(10)] + [
-                       'pearson_pvalue'] + ['pearson_coefficient'] + ["number_of_categories_selected"]
-    header_row3 = ["student_id"] + ["student_last_name"] + ["student_name"] + ["student_notes"] + ['rounds_1&2'] + [
-        'rounds_1&3'] + ['rounds_2&3'] + roundstring2 * 3
+    header_row2 = [''] * 4 + ["jdistances_vs_self_in_3_rounds"] + [''] * 2 + roundstring1 * 3
+    roundstring2 = [str(a_cell) for a_cell in google_categories] + [str(i + 1) for i in range(10)] + \
+                   ['jdistance_vs_google'] + [str(i + 1) for i in range(10)] + ['pearson_pvalue'] + \
+                   ['pearson_coefficient'] + ["number_of_categories_selected"]
+    header_row3 = ["student_id"] + ["student_last_name"] + ["student_name"] + ["student_notes"] + ['rounds_1&2'] + \
+                  ['rounds_1&3'] + ['rounds_2&3'] + roundstring2 * 3
 
-    return_list=header_row1+newrow_cell
-    return_list+=header_row2+newrow_cell
-    return_list+=header_row3+newrow_cell
+    return_list = header_row1+newrow_cell
+    return_list += header_row2+newrow_cell
+    return_list += header_row3+newrow_cell
 
     # constructing the table rows with the data in students
     for student in students:
         current_row = list()
-        current_row += [str(student['student_id'])] + [str(student['student_last_name'])] + [str(student['student_name'])] + [
-            student['student_notes']]
+        current_row += [str(student['student_id'])] + [str(student['student_last_name'])] + \
+                       [str(student['student_name'])] + [student['student_notes']]
         jdistances = student['jdistances']
         current_row += [str(jdistances[0]['(1,2)'])] + [str(jdistances[1]['(1,3)'])] + [str(jdistances[2]['(2,3)'])]
         for a_round in student['rounds']:
